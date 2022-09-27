a975eaa0fc4ead69627b3d94288c8d5900097fe5
JuChan <ghkdwncks96@gmail.com>
2022-09-27 19:10:03 +0900

    WWP-1627(DeleteModalButton): 컴포넌트 정형화 (#16)

--- src/stories/organisms/DeleteModalOpener/DeleteModalButton.stories.tsx
+++ src/stories/organisms/DeleteModalOpener/DeleteModalButton.stories.tsx
@@ -1,14 +1,41 @@
-import { ComponentStory, ComponentMeta } from "@storybook/react";
-import { DeleteModalOpener } from "./DeleteModalButton";
-import React from "react";
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { DeleteModalOpener } from './DeleteModalButton';
+import React from 'react';
 
 export default {
-  title: "organisms/DeleteModalOpener",
+  title: 'organisms/DeleteModalOpener',
   component: DeleteModalOpener,
 } as ComponentMeta<typeof DeleteModalOpener>;
 
 const DeleteModalOpenerStory: ComponentStory<typeof DeleteModalOpener> = (
-  args
-) => <DeleteModalOpener {...args} />;
+  args,
+) => <DeleteModalOpener
+  {...args}
+  title={(
+    <>
+      작성중인 내용이 사라집니다.
+      <br />
+      정말로 삭제 하시겠습니까?
+    </>
+  )}
+/>;
 
 export const Default = DeleteModalOpenerStory.bind({});
+Default.args = {
+  onDelete: () => {},
+  modalType: 'center',
+  type: 'button',
+  openerContents: '삭제',
+  confirmButtonTheme: 'wewin-peach500',
+  maxWidth: '340px',
+  title: (
+    <>
+      작성중인 내용이 사라집니다.
+      <br />
+      정말로 삭제 하시겠습니까?
+    </>
+  ),
+  disabled: false,
+  confirmButtonDisabled: false,
+  contour: false,
+};
