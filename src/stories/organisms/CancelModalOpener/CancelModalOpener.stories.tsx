1721eefa59c102e43707131667c9f537ad5b822d
JuChan <ghkdwncks96@gmail.com>
2022-09-27 19:09:42 +0900

    WWP-1622(CancelModalOpener): 컴포넌트 정형화 (#15)
    * feat(CancelModalOpener): 컴포넌트 정형화
    
    * feat(CancelModalOpener): openerContents 선언 위치 변경
    
    * refactor(CancelModalOpener): props 값 수정

--- src/stories/organisms/CancelModalOpener/CancelModalOpener.stories.tsx
+++ src/stories/organisms/CancelModalOpener/CancelModalOpener.stories.tsx
@@ -1,14 +1,31 @@
-import { ComponentStory, ComponentMeta } from "@storybook/react";
-import { CancelModalOpener } from "./CancelModalOpener";
-import React from "react";
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { CancelModalOpener } from './CancelModalOpener';
+import React from 'react';
 
 export default {
-  title: "organisms/CancelModalOpener",
+  title: 'organisms/CancelModalOpener',
   component: CancelModalOpener,
 } as ComponentMeta<typeof CancelModalOpener>;
 
 const CancelModalOpenerStory: ComponentStory<typeof CancelModalOpener> = (
-  args
-) => <CancelModalOpener {...args} />;
+  args,
+) => <CancelModalOpener
+  openerContents="Cancel Modal Opener"
+  {...args}
+/>;
 
 export const Default = CancelModalOpenerStory.bind({});
+Default.args = {
+  onCancel: () => {},
+  modalType: 'center',
+  variant: 'contain',
+  theme: 'wewin-blue600',
+  shape: 'square',
+  size: 'medium',
+  confirmButtonContents: '네, 취소합니다.',
+  confirmButtonTheme: 'wewin-peach500',
+  fontWeight: 'bold',
+  fontSize: 'medium',
+  fitContainer: false,
+};
+
