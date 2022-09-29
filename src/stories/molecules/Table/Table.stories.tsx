271d10987742620f9fa82503218df431f8c906cd
JuChan <ghkdwncks96@gmail.com>
2022-09-29 10:09:03 +0900

    WWP-1628(Table): 컴포넌트 스토리 문서 정형화 (#19)
    * feat(Table): 컴포넌트 스토리 문서 정형화
    
    * refactor(_palette): palette 색상 수정
    
    * refactor(Textbox): 스토리 작성
    
    * refactor(Table): border 보이지 않는 문제 해결

--- src/stories/molecules/Table/Table.stories.tsx
+++ src/stories/molecules/Table/Table.stories.tsx
@@ -1,9 +1,9 @@
-import { ComponentStory, ComponentMeta } from "@storybook/react";
-import { Table } from "./Table";
-import React from "react";
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Table } from './Table';
+import React from 'react';
 
 export default {
-  title: "molecules/Table",
+  title: 'molecules/Table',
   component: Table.Container,
 } as ComponentMeta<typeof Table.Container>;
 
@@ -12,17 +12,86 @@ const TableStory: ComponentStory<typeof Table.Container> = (args) => (
 );
 
 export const Default = TableStory.bind({});
-Default.args = {
-  children: (
-    <>
-      <Table.Head>
-        <Table.Row>
-          <Table.Title>title</Table.Title>
-          <Table.Title>title2</Table.Title>
-          <Table.Title>title3</Table.Title>
-        </Table.Row>
-      </Table.Head>
-      <Table.Body></Table.Body>
-    </>
-  ),
+Default.argTypes = {
+  children: {
+    defaultValue: (
+      <>
+        <Table.Head>
+          <Table.Row>
+            <Table.Title>title</Table.Title>
+            <Table.Title>title2</Table.Title>
+            <Table.Title>title3</Table.Title>
+          </Table.Row>
+        </Table.Head>
+        <Table.Body></Table.Body>
+      </>
+    ),
+    control: {
+      type: 'object',
+    },
+    table: {
+      type: {
+        summary: 'ReactNode',
+      },
+      defaultValue: {
+        summary: 'undefined',
+      },
+    },
+  },
+  dataExisted: {
+    defaultValue: false,
+    control: {
+      type: 'boolean',
+    },
+    table: {
+      type: {
+        summary: 'boolean',
+      },
+      defaultValue: {
+        summary: false,
+      },
+    },
+  },
+  loading: {
+    defaultValue: false,
+    control: {
+      type: 'boolean',
+    },
+    table: {
+      type: {
+        summary: 'boolean',
+      },
+      defaultValue: {
+        summary: false,
+      },
+    },
+  },
+  invalid: {
+    defaultValue: true,
+    control: {
+      type: 'boolean',
+    },
+    table: {
+      type: {
+        summary: 'boolean',
+      },
+      defaultValue: {
+        summary: true,
+      },
+    },
+  },
+  maxWidth: {
+    defaultValue: '100%',
+    control: {
+      type: 'text',
+    },
+    table: {
+      type: {
+        summary: 'React.CSSProperties["maxWidth"]',
+      },
+      defaultValue: {
+        summary: '100%',
+      },
+    },
+  },
 };
