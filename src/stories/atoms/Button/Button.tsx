28cda66756c05b59dbc605fa92fe87079b2fe397
HyeokjaeLee <leehyeokjae97@gmail.com>
2022-09-29 11:16:19 +0900

    fix(Button): variant prop 네이밍 변경
    - text > ghost

--- src/stories/atoms/Button/Button.tsx
+++ src/stories/atoms/Button/Button.tsx
@@ -1,31 +1,33 @@
-import scss from './Button.module.scss';
-import { cleanClassName } from '@utils';
-import { Link } from 'react-router-dom';
-import { omit } from 'lodash-es';
-import { useEffect, useState } from 'react';
-import type { IconProps } from 'react-feather';
+import scss from "./Button.module.scss";
+import { cleanClassName } from "@utils";
+import { Link } from "react-router-dom";
+import { omit } from "lodash-es";
+import { useEffect, useState } from "react";
+import type { IconProps } from "react-feather";
 
 export type ButtonTheme =
-  | 'wewin-blue100'
-  | 'wewin-blue600'
-  | 'bluish-gray50'
-  | 'bluish-gray100'
-  | 'bluish-gray200'
-  | 'bluish-gray500'
-  | 'bluish-gray600'
-  | 'wewin-peach500'
+  | "wewin-blue100"
+  | "wewin-blue600"
+  | "bluish-gray50"
+  | "bluish-gray100"
+  | "bluish-gray200"
+  | "bluish-gray500"
+  | "bluish-gray600"
+  | "wewin-peach500";
 
-export type Variant = 'contain' | 'outline' | 'text';
+export type Variant = "contain" | "outline" | "ghost";
 
 export interface ButtonProps
-  extends Pick<React.ButtonHTMLAttributes<HTMLButtonElement | HTMLAnchorElement>,
-    'onClick' | 'type' | 'children' | 'disabled' | 'name' | 'id'> {
+  extends Pick<
+    React.ButtonHTMLAttributes<HTMLButtonElement | HTMLAnchorElement>,
+    "onClick" | "type" | "children" | "disabled" | "name" | "id"
+  > {
   variant?: Variant;
   theme?: ButtonTheme;
-  shape?: 'round' | 'square';
-  size?: 'small' | 'medium' | 'large';
-  fontSize?: 'smallX' | 'small' | 'medium' | 'large' | 'large2X' | 'large4X';
-  fontWeight?: 'normal' | 'bold' | 'boldest';
+  shape?: "round" | "square";
+  size?: "small" | "medium" | "large";
+  fontSize?: "smallX" | "small" | "medium" | "large" | "large2X" | "large4X";
+  fontWeight?: "normal" | "bold" | "boldest";
   fitContainer?: boolean;
   minWidth?: string;
   to?: string;
@@ -34,11 +36,11 @@ export interface ButtonProps
 }
 export function Button({
   to,
-  variant = 'contain',
-  shape = 'square',
-  theme = 'wewin-blue600',
-  size = 'medium',
-  type = 'button',
+  variant = "contain",
+  shape = "square",
+  theme = "wewin-blue600",
+  size = "medium",
+  type = "button",
   delay,
   minWidth,
   icon: Icon,
@@ -67,13 +69,13 @@ export function Button({
       </div>
     ),
     className: cleanClassName(
-      `${scss.button} ${scss['theme_' + variant + '-' + theme]}
-      ${scss['size_' + size]}
-      ${scss['shape_' + shape]} 
-      ${scss['font_size_' + restProps.fontSize]}
-      ${scss['font_weight_' + restProps.fontWeight]}
+      `${scss.button} ${scss["theme_" + variant + "-" + theme]}
+      ${scss["size_" + size]}
+      ${scss["shape_" + shape]} 
+      ${scss["font_size_" + restProps.fontSize]}
+      ${scss["font_weight_" + restProps.fontWeight]}
       ${isDelaying && scss.delay_button}
-      ${restProps.fitContainer && scss.fit_container}`,
+      ${restProps.fitContainer && scss.fit_container}`
     ),
     style: {
       minWidth,
@@ -85,7 +87,7 @@ export function Button({
       <button {...buttonProps} disabled type={type}>
         <div
           className={cleanClassName(
-            `${scss.delaying_bar} ${startDelaying && scss.delaying}`,
+            `${scss.delaying_bar} ${startDelaying && scss.delaying}`
           )}
           style={{ transitionDuration: `${delay / 1000}s` }}
         />
@@ -93,6 +95,6 @@ export function Button({
       </button>
     );
   }
-  if (to) return <Link {...omit(buttonProps, ['disabled', 'name'])} to={to} />;
+  if (to) return <Link {...omit(buttonProps, ["disabled", "name"])} to={to} />;
   return <button {...buttonProps} type={type} />;
 }
