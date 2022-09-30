bcdd089a748bafb1f9c31b44450fece41362278d
JuChan <ghkdwncks96@gmail.com>
2022-09-30 15:06:06 +0900

    WWP-1541(Progress): Progress 컴포넌트 개발 (#23)
    * feat(_palette): peach color 추가
    
    * feat(colorUtil): color palette ts 파일 추가, colorUtil 유틸 함수 추가
    
    * feat(Progress): Progress 컴포넌트 개발
    
    * feat(index): Progress 컴포넌트 export

--- src/stories/atoms/index.tsx
+++ src/stories/atoms/index.tsx
@@ -1,83 +1,86 @@
-export { Button } from "./Button/Button";
-export type { ButtonProps } from "./Button/Button";
+export { Button } from './Button/Button';
+export type { ButtonProps } from './Button/Button';
 
-export { Checkbox } from "./Checkbox/Checkbox";
-export type { CheckboxProps } from "./Checkbox/Checkbox";
+export { Checkbox } from './Checkbox/Checkbox';
+export type { CheckboxProps } from './Checkbox/Checkbox';
 
-export { DateSelectbox } from "./DateSelectbox/DateSelectbox";
+export { DateSelectbox } from './DateSelectbox/DateSelectbox';
 export type {
   DateSelectboxProps,
   Period,
   DateType,
-} from "./DateSelectbox/DateSelectbox";
+} from './DateSelectbox/DateSelectbox';
 
-export { EditStamp } from "./EditStamp/EditStamp";
-export type { EditStampProps } from "./EditStamp/EditStamp";
+export { EditStamp } from './EditStamp/EditStamp';
+export type { EditStampProps } from './EditStamp/EditStamp';
 
-export { ExpandableButton } from "./ExpandableButton/ExpandableButton";
-export type { ExpandableButtonProps } from "./ExpandableButton/ExpandableButton";
+export { ExpandableButton } from './ExpandableButton/ExpandableButton';
+export type { ExpandableButtonProps } from './ExpandableButton/ExpandableButton';
 
-export { Hr } from "./Hr/Hr";
-export type { HrProps } from "./Hr/Hr";
+export { Hr } from './Hr/Hr';
+export type { HrProps } from './Hr/Hr';
 
-export { Label } from "./Label/Label";
-export type { LabelProps } from "./Label/Label";
+export { Label } from './Label/Label';
+export type { LabelProps } from './Label/Label';
 
-export { Loading } from "./Loading/Loading";
+export { Loading } from './Loading/Loading';
 
-export { Modal } from "./Modal/Modal";
-export type { ModalProps } from "./Modal/Modal";
+export { Modal } from './Modal/Modal';
+export type { ModalProps } from './Modal/Modal';
 
-export { MonthSelectbox } from "./MonthSelectbox/MonthSelectbox";
+export { MonthSelectbox } from './MonthSelectbox/MonthSelectbox';
 export type {
   MonthSelectboxProps,
   MonthDate,
-} from "./MonthSelectbox/MonthSelectbox";
+} from './MonthSelectbox/MonthSelectbox';
 
-export { Pagination } from "./Pagination/Pagination";
-export type { PaginationProps } from "./Pagination/Pagination";
+export { Pagination } from './Pagination/Pagination';
+export type { PaginationProps } from './Pagination/Pagination';
 
-export { Radiobox } from "./Radiobox/Radiobox";
-export type { RadioboxProps } from "./Radiobox/Radiobox";
+export { Radiobox } from './Radiobox/Radiobox';
+export type { RadioboxProps } from './Radiobox/Radiobox';
 
-export { Searchbox } from "./Searchbox/Searchbox";
-export type { SearchboxProps } from "./Searchbox/Searchbox";
+export { Searchbox } from './Searchbox/Searchbox';
+export type { SearchboxProps } from './Searchbox/Searchbox';
 
-export { SectionMoveButton } from "./SectionMoveButton/SectionMoveButton";
-export type { SectionMoveButtonProps } from "./SectionMoveButton/SectionMoveButton";
+export { SectionMoveButton } from './SectionMoveButton/SectionMoveButton';
+export type { SectionMoveButtonProps } from './SectionMoveButton/SectionMoveButton';
 
-export { SectionSlider } from "./SectionSlider/SectionSlider";
+export { SectionSlider } from './SectionSlider/SectionSlider';
 export type {
   SectionSliderContainerProps,
   SectionSliderItemProps,
-} from "./SectionSlider/SectionSlider";
+} from './SectionSlider/SectionSlider';
 
-export { SectionSwitch } from "./SectionSwitch/SectionSwitch";
+export { SectionSwitch } from './SectionSwitch/SectionSwitch';
 export type {
   SectionSwitchContainerProps,
   SectionSwitchItemProps,
-} from "./SectionSwitch/SectionSwitch";
+} from './SectionSwitch/SectionSwitch';
 
-export { Selectbox } from "./Selectbox/Selectbox";
-export type { SelectboxProps, OptionHint } from "./Selectbox/Selectbox";
+export { Selectbox } from './Selectbox/Selectbox';
+export type { SelectboxProps, OptionHint } from './Selectbox/Selectbox';
 
-export { Switch } from "./Switch/Switch";
-export type { SwitchProps } from "./Switch/Switch";
+export { Switch } from './Switch/Switch';
+export type { SwitchProps } from './Switch/Switch';
 
-export { Tag } from "./Tag/Tag";
-export type { TagProps } from "./Tag/Tag";
+export { Tag } from './Tag/Tag';
+export type { TagProps } from './Tag/Tag';
 
-export { Tagbox } from "./Tagbox/Tagbox";
-export type { TagboxProps } from "./Tagbox/Tagbox";
+export { Tagbox } from './Tagbox/Tagbox';
+export type { TagboxProps } from './Tagbox/Tagbox';
 
-export { Textarea } from "./Textarea/Textarea";
-export type { TextareaProps } from "./Textarea/Textarea";
+export { Textarea } from './Textarea/Textarea';
+export type { TextareaProps } from './Textarea/Textarea';
 
-export { Textbox } from "./Textbox/Textbox";
-export type { TextboxProps } from "./Textbox/Textbox";
+export { Textbox } from './Textbox/Textbox';
+export type { TextboxProps } from './Textbox/Textbox';
 
-export { Tooltip } from "./Tooltip/Tooltip";
-export type { TooltipProps } from "./Tooltip/Tooltip";
+export { Tooltip } from './Tooltip/Tooltip';
+export type { TooltipProps } from './Tooltip/Tooltip';
 
-export { ValidationMessage } from "./ValidationMessage/ValidationMessage";
-export type { ValidationMessageProps } from "./ValidationMessage/ValidationMessage";
+export { ValidationMessage } from './ValidationMessage/ValidationMessage';
+export type { ValidationMessageProps } from './ValidationMessage/ValidationMessage';
+
+export { Progress } from './Progress/Progress';
+export type { ProgressProps } from './Progress/Progress';
---
+++ src/utils/palette.ts
@@ -0,0 +1,74 @@
+export const colors = {
+  //Gray
+  gray3: '#828282',
+  gray4: '#bdbdbd',
+  gray10: '#f7f7f7',
+  gray50: '#f2f2f2',
+  gray100: '#f0f0f0',
+  gray200: '#e6e6e6',
+  gray300: '#d6d6d6',
+  gray400: '#c2c2c2',
+  gray500: '#a3a3a3',
+  gray600: '#858585',
+  gray700: '#5C5C5C',
+  gray800: '#333333',
+  gray900: '#141414',
+
+  //Bluish Gray
+  bluishGray10: '#f5f7f9',
+  bluishGray50: '#f2f4f7',
+  bluishGray100: '#eceef4',
+  bluishGray200: '#dfe4ec',
+  bluishGray300: '#ccd3e0',
+  bluishGray400: '#b2bdd1',
+  bluishGray500: '#8c9cba',
+  bluishGray600: '#667ba3',
+  bluishGray700: '#455573',
+  bluishGray800: '#262f40',
+  bluishGray900: '#0f1319',
+
+//Wewin Blue
+  wewinBlue10: '#f0f4ff',
+  wewinBlue50: '#ebf0ff',
+  wewinBlue100: '#e0e9ff',
+  wewinBlue200: '#ccdaff',
+  wewinBlue300: '#adc3ff',
+  wewinBlue400: '#85a5ff',
+  wewinBlue500: '#4778ff',
+  wewinBlue600: '#0a4bff',
+  wewinBlue700: '#0031b8',
+  wewinBlue800: '#001b66',
+  wewinBlue900: '#000b29',
+
+//Wewin Gold
+  wewinGold200: '#fff3cc',
+  wewinGold700: '#b88d00',
+
+  //Wewin Orange
+  wewinOrange10: '#fff7f0',
+  wewinOrange50: '#fff5eb',
+  wewinOrange100: '#fff0e0',
+  wewinOrange200: '#ffe5cc',
+  wewinOrange300: '#ffd6ad',
+  wewinOrange400: '#ffc285',
+  wewinOrange500: '#ffa347',
+  wewinOrange600: '#ff850a',
+  wewinOrange700: '#b85c00',
+  wewinOrange800: '#663300',
+
+//Peach
+  peach50: '#ffeceb',
+  peach100: '#ffe2e0',
+  peach200: '#ffcfcc',
+  peach300: '#ffb3ad',
+  peach400: '#ff8d85',
+  peach500: '#ff5447',
+  peach600: '#fd1808',
+
+//Wewin Peach
+  wewinPeach500: '#ff5447',
+  wewinPeach600: '#fd1808',
+};
+
+export type Color = keyof typeof colors;
+
