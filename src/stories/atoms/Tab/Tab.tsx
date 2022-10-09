5b4748e418023ea89bd18fbd163fdedd4e9d5800
HyeokjaeLee <leehyeokjae97@gmail.com>
2022-10-10 00:22:37 +0900

    hotfix(Tab): 활성 조건 수정
    - 쿼리스트링 값을 제외하도록 변경

--- src/stories/atoms/Tab/Tab.tsx
+++ src/stories/atoms/Tab/Tab.tsx
@@ -1,6 +1,6 @@
-import scss from "./Tab.module.scss";
-import { cleanClassName } from "@utils";
-import { Link, useLocation } from "react-router-dom";
+import scss from './Tab.module.scss';
+import { cleanClassName } from '@utils';
+import { Link, useLocation } from 'react-router-dom';
 
 export interface TabProps {
   items: { label: React.ReactNode; to: string }[];
@@ -11,14 +11,12 @@ export function Tab({ items }: TabProps) {
     <nav className={scss.tab}>
       <ul className={scss.tab_list}>
         {items.map(({ label, to }) => {
-          const isActive = currentPath === to;
+          const isActive = currentPath === to.split('?')[0];
           return (
             <li className={cleanClassName(isActive && scss.active)}>
               <Link
                 to={to}
-                className={cleanClassName(
-                  `${scss.tab_link} ${isActive && scss.active}`
-                )}
+                className={cleanClassName(`${scss.tab_link} ${isActive && scss.active}`)}
               >
                 {label}
               </Link>
