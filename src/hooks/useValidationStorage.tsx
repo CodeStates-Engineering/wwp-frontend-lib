87b78acdeda8fa1bfafe726e82a4fd21be1459ba
HyeokjaeLee <leehyeokjae97@gmail.com>
2022-10-12 17:27:32 +0900

    hotfix(useValidationStorage): 버그 수정
    - Storage가 정상적으로 메모제이션되지 않는 버그 수정

--- src/hooks/useValidationStorage.tsx
+++ src/hooks/useValidationStorage.tsx
@@ -1,16 +1,19 @@
 import { useMemo } from 'react';
 
 export function useValidationStorage() {
-  const validationMap = useMemo(() => new Map<string, () => boolean>(), []);
-  return {
-    check: () => {
-      const invalidKeys: string[] = [];
-      for (const [key, check] of validationMap) if (!check()) invalidKeys.push(key);
-      return invalidKeys;
-    },
-    set: (key: string, value: () => boolean) => validationMap.set(key, value),
-    delete: (key: string) => validationMap.delete(key),
-  };
+  return useMemo(() => {
+    const validationMap = new Map<string, () => boolean>();
+    return {
+      data: validationMap,
+      check: () => {
+        const invalidKeys: string[] = [];
+        for (const [key, check] of validationMap) if (!check()) invalidKeys.push(key);
+        return invalidKeys;
+      },
+      set: (key: string, value: () => boolean) => validationMap.set(key, value),
+      delete: (key: string) => validationMap.delete(key),
+    };
+  }, []);
 }
 
 export type ValidationStorage = ReturnType<typeof useValidationStorage>;
