b7be526ad2b882a48d2721e21020045af416ccac
juchanhwang <ghkdwncks96@gmail.com>
2022-10-23 18:58:50 +0900

    fix(Progress): Progress 컴포넌트 재설계 및 개발

--- src/components/atoms/Progress/Progress.tsx
+++ src/components/atoms/Progress/Progress.tsx
@@ -1,71 +1,39 @@
-import { cleanClassName, Color } from '../../../utils';
+import { cleanClassName } from '../../../utils';
 import scss from './Progress.module.scss';
-import { colorUtil } from '../../../utils/colorUtil';
+
+type Color = 'green600' | 'peach400' | 'wewin-orange500' | 'wewin-blue300';
 
 export interface ProgressProps {
-  indicatorCount?: number;
-  width?: React.CSSProperties['width'];
-  trackColor?: Color;
-
-  indicatorNum?: number;
-  indicatorColor?: Color;
-
-  subIndicatorColor?: Color;
-  subIndicatorNum?: number;
-
-  lastIndicatorColor?: Color;
-  lastIndicatorNum?: number;
+  totalCount: number;
+  indicatorConfig?: {
+    color: Color;
+    activeNumber: number[];
+  }[];
 }
 export function Progress({
-  indicatorCount = 10,
-  trackColor = 'bluishGray50',
-  width,
-  indicatorNum = 0,
-  indicatorColor = 'wewinBlue600',
-  subIndicatorNum = 0,
-  subIndicatorColor = 'peach400',
-  lastIndicatorNum = 0,
-  lastIndicatorColor = 'wewinOrange500',
+  totalCount,
+  indicatorConfig,
 }: ProgressProps) {
-  const parsedTrackColor = colorUtil.parse(trackColor);
-  const parsedIndicatorColor = colorUtil.parse(indicatorColor);
-  const parsedSubIndicatorColor = colorUtil.parse(subIndicatorColor);
-  const parsedLastIndicatorColor = colorUtil.parse(lastIndicatorColor);
-
   return (
     <div
       role="progressbar"
       className={cleanClassName(`${scss.track}`)}
-      style={{
-        width,
-        backgroundSize: `${100 / indicatorCount}%`,
-        backgroundImage: `repeating-linear-gradient(to right, #ccd3e0, #ccd3e0 1px, ${parsedTrackColor} 1px, ${parsedTrackColor})`,
-      }}
     >
-      <div
-        className={cleanClassName(`${scss.indicator}`)}
-        style={{
-          width: `${(indicatorNum / indicatorCount) * 100}%`,
-          backgroundSize: `${100 / indicatorNum}%`,
-          backgroundImage: `repeating-linear-gradient(to right, #ccd3e0, #ccd3e0 1px, ${parsedIndicatorColor} 1px, ${parsedIndicatorColor})`,
-        }}
-      ></div>
-      <div
-        className={cleanClassName(`${scss.sub_indicator}`)}
-        style={{
-          width: `${(subIndicatorNum / indicatorCount) * 100}%`,
-          backgroundSize: `${100 / subIndicatorNum}%`,
-          backgroundImage: `repeating-linear-gradient(to right, #ccd3e0, #ccd3e0 1px, ${parsedSubIndicatorColor} 1px, ${parsedSubIndicatorColor})`,
-        }}
-      ></div>
-      <div
-        className={cleanClassName(`${scss.last_indicator}`)}
-        style={{
-          width: `${(lastIndicatorNum / indicatorCount) * 100}%`,
-          backgroundSize: `${100 / lastIndicatorNum}%`,
-          backgroundImage: `repeating-linear-gradient(to right, #ccd3e0, #ccd3e0 1px, ${parsedLastIndicatorColor} 1px, ${parsedLastIndicatorColor})`,
-        }}
-      ></div>
+      {
+        [...Array(totalCount)].map((_, idx) => (
+            <div
+              key={idx}
+              className={cleanClassName(`
+              ${scss.indicator} 
+              ${indicatorConfig && scss[indicatorConfig?.filter((item) => item.activeNumber.includes(idx + 1)).map((item) => item.color)[0]]}
+              `)}
+              style={{
+                width: `${100 / totalCount}%`,
+              }}
+            ></div>
+          ),
+        )
+      }
     </div>
   );
 }
