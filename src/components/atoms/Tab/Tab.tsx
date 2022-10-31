a9b20855fea781cd611877dd1d1a776a4e84207a
HyeokjaeLee <leehyeokjae97@gmail.com>
2022-10-31 14:15:35 +0900

    feat: Next.js 지원 (#56)

--- src/components/atoms/Tab/Tab.tsx
+++ src/components/atoms/Tab/Tab.tsx
@@ -1,12 +1,40 @@
 import scss from './Tab.module.scss';
 import { cleanClassName } from '../../../utils';
-import { Link, useLocation } from 'react-router-dom';
 
 export interface TabProps {
   items: { label: React.ReactNode; to: string }[];
 }
 export function Tab({ items }: TabProps) {
-  const currentPath = useLocation().pathname;
+  const currentPath: string = (() => {
+    try {
+      const { useRouter } = require('next/router');
+      return useRouter().pathname;
+    } catch (e) {
+      const { useLocation } = require('react-router-dom');
+      return useLocation().pathname;
+    }
+  })();
+  const Link = ({
+    isActive,
+    children,
+    to,
+  }: {
+    isActive: boolean;
+    children: React.ReactNode;
+    to: string;
+  }) => {
+    const commonProps = {
+      className: cleanClassName(`${scss.tab_link} ${isActive && scss.active}`),
+      children,
+    };
+    try {
+      const Link = require('next/link').default;
+      return <Link {...commonProps} href={to}></Link>;
+    } catch (e) {
+      const { Link } = require('react-router-dom');
+      return <Link {...commonProps} to={to} />;
+    }
+  };
   return (
     <nav className={scss.tab}>
       <ul className={scss.tab_list}>
@@ -14,10 +42,7 @@ export function Tab({ items }: TabProps) {
           const isActive = currentPath === to.split('?')[0];
           return (
             <li className={cleanClassName(isActive && scss.active)} key={`${label}_${index}`}>
-              <Link
-                to={to}
-                className={cleanClassName(`${scss.tab_link} ${isActive && scss.active}`)}
-              >
+              <Link to={to} isActive={isActive}>
                 {label}
               </Link>
             </li>
