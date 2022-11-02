fd0dc5a00be3d7a5fcca9b4052781057dbd16efc
HyeokjaeLee <leehyeokjae97@gmail.com>
2022-11-02 15:09:50 +0900

    feat(usePathname): 플러그인 추가
    - Tab 컴포넌트의 pathname을 가져오는 방식 변경

--- src/plugins/index.tsx
+++ src/plugins/index.tsx
@@ -1,7 +1,10 @@
 import { Link, setLinkToNext } from './Link';
-export { Link };
 export type { LinkProps } from './Link';
+import { usePathname, setUsePathnameToNext } from './usePathname';
 
+export { Link, usePathname };
+/**react 용으로 개발된 컴포넌트들을 next 용으로 변경*/
 export function setupForNext() {
   setLinkToNext();
+  setUsePathnameToNext();
 }
